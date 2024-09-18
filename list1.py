ed89655f0089d0e07c0581a2d873081bab5d781e
kdw <system421@gmail.com>
2024-09-18 20:53:20 +0900

    first commit

---
+++ list1.py
@@ -0,0 +1,14 @@
+a = [1,2,3,4,5,6,7,8,9]
+print(a)
+
+print(a[4])
+
+a = list()
+print(a)
+
+a = []
+print(a)
+
+n = 10
+a = [0] * 10
+print(a)
---
+++ number2.py
@@ -0,0 +1,11 @@
+a = 157.93
+print(a)
+
+a = -1837.2
+print(a)
+
+a = 5.
+print(a)
+
+a = -.7
+print(a)
